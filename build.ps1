ac3ba84e82800a28f5d1573e05743760c2d5941c
まぅSmallYuan <alan6716s@gmail.com>
2023-09-16 23:00:45 +0800

    補充無限符號

--- build.ps1
+++ build.ps1
@@ -3,6 +3,11 @@ $Tag = [System.TimeZoneInfo]::ConvertTime($(Get-Item assets/tmpchinesefont).Last
 echo "MY_TAG=$Tag" | Out-File -FilePath $env:GITHUB_ENV -Append
 $Path = "Release"
 New-Item -Name "$Path" -ItemType "directory" -Force
-New-Item -Path "$Path" -Name "Mods" -ItemType "directory" -Force
-Copy-Item -Path assets/tmpchinesefont -Destination $Path/Mods -Force
-7z a -t7z "$Path/tmpchinesefont_$Tag.7z" "./$Path/*" -mx=9 -ms
+New-Item -Path "$Path" -Name "BepInEx" -ItemType "directory" -Force
+New-Item -Path "$Path/BepInEx" -Name "plugins" -ItemType "directory" -Force
+New-Item -Path "$Path/BepInEx/plugins" -Name "LLC" -ItemType "directory" -Force
+$BIE_LLC_Path = "$Path/BepInEx/plugins/LLC"
+Copy-Item -Path assets/tmpchinesefont $BIE_LLC_Path -Force
+Set-Location "$Path"
+7z a -t7z "./tmpchinesefont_BIE_$Tag.7z" "BepInEx/" -mx=9 -ms
+Set-Location "../"
